c8ef9263b343a7830ee13e7d435d7fa29bd5aa34
Cyrus Bigham <bighamcyrus@gmail.com>
2022-06-15 09:09:13 -0700

    javascript code added

---
+++ script.js
@@ -0,0 +1,60 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword(){
+  var LC = "abc"
+  var UC = "ABC"
+  var numeric ="123"
+  var special = "!@#"
+  var potential = ""
+  var password = ""
+ var passwordLength = prompt("How many characters do you want your password to be? Must be between 8-128 characters.")
+
+ if (passwordLength < 8 || passwordLength > 128) {
+  alert ("Password does not meet Character length Requirements")
+  return
+ }
+
+ var passwordLC = confirm("Do you want to include lower case letters")
+ var passwordUC = confirm("Do you want to include Uppercase?")
+ var numericValue = confirm("do you want to include numeric values?")
+ var specialcharacters = confirm("do you want to include special characters?")
+
+ if (!passwordLC && !passwordUC && !numericValue && !specialcharacters) {
+  alert("Must have at least one character type to meet password requirements")
+  return
+  
+  
+ }
+ if (passwordLC) {
+  potential+=LC
+   }
+  if (passwordUC){
+    potential+=UC
+   }
+   if (numericValue){
+    potential+=numeric
+   } 
+   if (specialcharacters) {
+    potential+=special
+   }
+
+   for (let i = 0; i < passwordLength; i++) {
+    var indexChar = Math.floor(Math.random() * potential.length)
+     var selectedChar = potential[indexChar]
+     password+=selectedChar
+   }
+   return password
+
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
